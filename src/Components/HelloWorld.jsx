06796a6d88ebca6b894793ec6c381a3f122e5494
Rithik Jain <rithik.jain3006@gmail.com>
2020-10-02 14:16:47 +0530

    Navigation with animation

--- src/Components/HelloWorld.jsx
+++ src/Components/HelloWorld.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 class HelloWorld extends React.Component {
   render() {
     return (
-    <h1>Hello {this.props.name}</h1>
+      <div className="p-3">
+        <span className="font-bold text-xl">
+          This is the home page
+        </span>
+        <h1>Hello {this.props.name}</h1>
+      </div>
     )
   }
 }
