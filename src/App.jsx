43220f2b29a7383e28d7ac4b815c75dd39a89be2
Rithik Jain <rithik.jain3006@gmail.com>
2020-10-02 16:30:55 +0530

    Meh done deal

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,37 @@ import React from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import HelloWorld from './Components/HelloWorld'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Header />
 
-      <HelloWorld name = "Rithik"/>
+      <HelloWorld name="Rithik" />
+
+      <Router>
+        <ul>
+          <li>
+            <Link to="/" className="text-blue-500">Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-blue-500">About</Link>
+          </li>
+        </ul>
+      </Router>
+
+      <Switch>
+        <Route path="/">
+          
+        </Route>
+        <Route path="/about"></Route>
+      </Switch>
 
       <Footer />
     </div>
